75971b57c6a2499b0eb4b6cba56f79637d7a4a3a
alvaro <adelcastifo@gmail.com>
2024-08-17 17:59:15 +0200

    Created  the Task data class in com.algsyntax.jetpackcomposetodo.data.model package and added Java-style documentation.

---
+++ ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/data/model/Task.kt
@@ -0,0 +1,16 @@
+package com.algsyntax.jetpackcomposetodo.data.model
+
+/**
+ * Represents a task in the ToDo application.
+ *
+ * @property id The unique identifier for the task.
+ * @property title The title of the task.
+ * @property description A brief description of the task.
+ * @property isCompleted Indicates whether the task has been completed. Defaults to false.
+ */
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean = false
+)
