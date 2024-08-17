c769453dd61291510c2a2da3fb3436c5be6b479c
alvaro <adelcastifo@gmail.com>
2024-08-17 18:08:07 +0200

    Added Java-style documentation for the TaskRepository class in com.algsyntax.todojetpackcompose.data.repository package and changed val isCompleted to var isCompleted , making it mutable.

--- ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/data/model/Task.kt
+++ ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/data/model/Task.kt
@@ -12,5 +12,5 @@ data class Task(
     val id: Int,
     val title: String,
     val description: String,
-    val isCompleted: Boolean = false
+    var isCompleted: Boolean = false
 )
--- ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/data/repository/TaskRepository.kt
+++ ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/data/repository/TaskRepository.kt
@@ -1,4 +1,41 @@
 package com.algsyntax.todojetpackcompose.data.repository
 
+import com.algsyntax.jetpackcomposetodo.data.model.Task
+
+/**
+ * Repository for managing tasks in the ToDo application.
+ *
+ * This class provides methods to retrieve, add, and complete tasks.
+ */
 class TaskRepository {
+    private val tasks = mutableListOf<Task>()
+
+    /**
+     * Retrieves the list of all tasks.
+     *
+     * @return A list of tasks.
+     */
+    fun getTasks(): List<Task> {
+        return tasks
+    }
+
+    /**
+     * Adds a new task to the repository.
+     *
+     * @param task The task to be added.
+     */
+    fun addTask(task: Task) {
+        tasks.add(task)
+    }
+
+    /**
+     * Marks the task with the given ID as completed.
+     *
+     * @param taskId The ID of the task to mark as completed.
+     */
+    fun completeTask(taskId: Int) {
+        tasks.find { it.id == taskId }?.let {
+            it.isCompleted = true
+        }
+    }
 }
