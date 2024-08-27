9b117d750f2f6bf6598d1eea57abd74bcc2e3354
alvaro <adelcastifo@gmail.com>
2024-08-27 06:56:40 +0200

    Removed unnecessary SDK version check for dynamic color scheme
    - Removed the redundant SDK version check in the theme configuration as the minimum SDK level is always 34.
    - Simplified the logic in `ToDoJetpackComposeTheme` by directly applying the dynamic color scheme without conditional checks.
    - This change streamlines the theme setup and eliminates unnecessary code.
    - The app now features a more vibrant, tech-forward look with neon accents, enhancing the overall user experience.

--- ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/ui/theme/Theme.kt
+++ ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.algsyntax.todojetpackcompose.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -57,7 +56,7 @@ fun ToDoJetpackComposeTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
