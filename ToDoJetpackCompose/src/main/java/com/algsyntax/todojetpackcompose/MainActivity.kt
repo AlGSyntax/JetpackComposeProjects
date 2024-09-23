e80d6ee771174110f271b0188dd39976565c8e41
alvaro <adelcastifo@gmail.com>
2024-09-23 10:33:24 +0200

    Added FLAG_SECURE to prevent screenshots and screen recordings
    - Imported WindowManager and set FLAG_SECURE in MainActivity
    - This change enhances security by preventing unauthorized capture of app content
    - Updated documentation to explain the purpose of the FLAG_SECURE flag

--- ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/MainActivity.kt
+++ ToDoJetpackCompose/src/main/java/com/algsyntax/todojetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.algsyntax.todojetpackcompose
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -11,12 +12,16 @@ import com.algsyntax.todojetpackcompose.ui.screens.TaskListScreen
 import com.algsyntax.todojetpackcompose.ui.theme.ToDoJetpackComposeTheme
 import com.algsyntax.todojetpackcompose.viewmodel.TaskViewModel
 
+
 /**
  * The main activity of the ToDoJetpackCompose app.
  *
  * This activity sets the content view using Jetpack Compose and applies the app's theme.
  * It uses a Scaffold layout to manage the structure of the UI and passes padding values
  * from the Scaffold to the TaskListScreen composable.
+ *
+ * Additionally, it sets the FLAG_SECURE flag to prevent screenshots and screen recordings,
+ * enhancing the security of user data.
  */
 class MainActivity : ComponentActivity() {
     // ViewModel instance for managing the tasks
@@ -24,6 +29,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Prevents screenshots and screen recordings for security reasons
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+
         setContent {
             ToDoJetpackComposeTheme {
                 Scaffold { paddingValues ->
